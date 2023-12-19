96bd1c4a1092c0f5be1773ef1c039b161a4428d7
Sushmitadasari <147468660+Sushmitadasari@users.noreply.github.com>
2023-12-19 00:30:51 -0500

    #include<stdio.h> int main() {     int a,i; 	scanf("%d",&a);     int arr[a]; 	for(i=0;i<a;i++) 	{ 		scanf("%d",&arr[i]); 	} 	int sum=0; 	for(i=0;i<a;i++) 	{ 	   sum=sum+arr[i]; 	} 	int count=0; 	int avg=sum/a; for(i=0;i<a;i++) { 	if(arr[i]>=avg) 	{ 	    count++; 	} } 	printf("%d",count);
    }

---
+++ Elements_greater_than_average.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int a,i;
+	scanf("%d",&a);
+    int arr[a];
+	for(i=0;i<a;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	int sum=0;
+	for(i=0;i<a;i++)
+	{
+	   sum=sum+arr[i];
+	}
+	int count=0;
+	int avg=sum/a;
+for(i=0;i<a;i++)
+{
+	if(arr[i]>=avg)
+	{
+	    count++;
+	}
+}
+	printf("%d",count);
+	
+}
